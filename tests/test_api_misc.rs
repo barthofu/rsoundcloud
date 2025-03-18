76ab0e9b90e3c2dde993183229a133a57efc055e
Bartholomé Gili <dev.bartho@gmail.com>
2025-03-18 11:25:59 +0100

    test: add missing `misc` tests

--- tests/test_api_misc.rs
+++ tests/test_api_misc.rs
@@ -1,11 +1,36 @@
 mod common;
 
 use common::get_client;
-use rsoundcloud::{CollectionParams, SearchApi};
+use rsoundcloud::MiscApi;
+
+const TRACK_URL: &str = "https://soundcloud.com/shmanii/beg-me-to-come-over";
+const USER_URL: &str = "https://soundcloud.com/shmanii";
+const PLAYLIST_URL: &str = "https://soundcloud.com/bartho-az/sets/future-guinguette";
 
 #[tokio::test]
-async fn client_can_search() {
+async fn client_can_resolve() {
     get_client().await
-        .search("shmanii".to_string(), CollectionParams::default())
+        .resolve(TRACK_URL)
+        .await.unwrap();
+}
+
+#[tokio::test]
+async fn client_can_get_resource() {
+    get_client().await
+        .resolve_track(TRACK_URL)
+        .await.unwrap();
+}
+
+#[tokio::test]
+async fn client_can_resolve_user() {
+    get_client().await
+        .resolve_user(USER_URL)
+        .await.unwrap();
+}
+
+#[tokio::test]
+async fn client_can_resolve_playlist() {
+    get_client().await
+        .resolve_album_playlist(PLAYLIST_URL)
         .await.unwrap();
 }
--- tests/test_api_playlists.rs
+++ tests/test_api_playlists.rs
@@ -3,41 +3,41 @@ mod common;
 use common::get_client;
 use rsoundcloud::{ResourceId, PlaylistsApi};
 
-const USER_ID: ResourceId = ResourceId::Id(1847484585);
-const TRACK_URL: &str = "https://soundcloud.com/bartho-az/sets/future-guinguette";
-const TRACK_URI: &str = "bartho-az/sets/future-guinguette";
+const PLAYLIST_ID: ResourceId = ResourceId::Id(1847484585);
+const PLAYLIST_URL: &str = "https://soundcloud.com/bartho-az/sets/future-guinguette";
+const PLAYLIST_URI: &str = "bartho-az/sets/future-guinguette";
 
 #[tokio::test]
 async fn client_can_get_playlist_from_id() {
     get_client().await
-        .get_playlist(USER_ID)
+        .get_playlist(PLAYLIST_ID)
         .await.unwrap();
 }
 
 #[tokio::test]
 async fn client_can_get_playlist_from_url() {
     get_client().await
-        .get_playlist(ResourceId::Url(TRACK_URL.to_string()))
+        .get_playlist(ResourceId::Url(PLAYLIST_URL.to_string()))
         .await.unwrap();
 }
 
 #[tokio::test]
 async fn client_can_get_playlist_from_uri() {
     get_client().await
-        .get_playlist(ResourceId::Uri(TRACK_URI.to_string()))
+        .get_playlist(ResourceId::Uri(PLAYLIST_URI.to_string()))
         .await.unwrap();
 }
 
 #[tokio::test]
 async fn client_can_get_playlist_likers() {
     get_client().await
-        .get_playlist_likers(USER_ID)
+        .get_playlist_likers(PLAYLIST_ID)
         .await.unwrap();
 }
 
 #[tokio::test]
 async fn client_can_get_playlist_reposters() {
     get_client().await
-        .get_playlist_reposters(USER_ID)
+        .get_playlist_reposters(PLAYLIST_ID)
         .await.unwrap();
 }
